067dfecf47becdff4a59de25c2b593fcca7e448b
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-03-01 22:00:03 -0600

    feat(course): create course entity

---
+++ src/main/java/com/joelk/forohub/domain/course/Category.java
@@ -0,0 +1,9 @@
+package com.joelk.forohub.domain.course;
+
+public enum Category {
+    BACKEND,
+    FRONTEND,
+    DATA_SCIENCE,
+    MOBILE,
+    SOFT_SKILLS
+}
---
+++ src/main/java/com/joelk/forohub/domain/course/Course.java
@@ -0,0 +1,26 @@
+package com.joelk.forohub.domain.course;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "courses")
+@Entity(name = "course")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+}
