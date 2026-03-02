254b7aaec27c3a85aa615f8ea9834c827fa8f941
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-03-02 11:56:36 -0600

    feat(db): add data for courses

---
+++ src/main/resources/db/migration/V3__insert-elements-in-table-courses.sql
@@ -0,0 +1,3 @@
+INSERT INTO courses (name, category) VALUES ('Java Beginner', 'BACKEND');
+INSERT INTO courses (name, category) VALUES ('Spring Boot Framework', 'BACKEND');
+INSERT INTO courses (name, category) VALUES ('PostgreSQL Databases', 'DATA_SCIENCE');
