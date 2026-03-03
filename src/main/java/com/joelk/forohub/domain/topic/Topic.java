bc6964b5af013f5afda9a3d0487678fba42bb761
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-03-03 16:42:39 -0600

    feat(topic): create topic entity

---
+++ src/main/java/com/joelk/forohub/domain/topic/Topic.java
@@ -0,0 +1,48 @@
+package com.joelk.forohub.domain.topic;
+
+import com.joelk.forohub.domain.course.Course;
+import com.joelk.forohub.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "topics", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_topic_title_message", columnNames = {"title", "message"})
+})
+@Entity(name = "Topic")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+}
