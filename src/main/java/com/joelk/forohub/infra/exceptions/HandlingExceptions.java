62a3ee625f51fb0d59c9e874e6fff19b7c8ceeda
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-03-04 10:29:19 -0600

    feat(infra/exceptions): add RestControllerAdvice to handle common API errors

---
+++ src/main/java/com/joelk/forohub/infra/exceptions/HandlingExceptions.java
@@ -0,0 +1,35 @@
+package com.joelk.forohub.infra.exceptions;
+
+import com.joelk.forohub.domain.ValidationException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HandlingExceptions {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404NotFound() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity erro400Client(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DataValidationError::new).toList());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity handleBusinessLogicError(ValidationException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    public record DataValidationError(String field, String message) {
+        public DataValidationError(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
