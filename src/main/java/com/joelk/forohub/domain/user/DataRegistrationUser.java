018aca8c6796baef2e3bc8622378f8161c7e5e60
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-03-23 14:14:49 -0600

    feat(user): add DataRegistration dto

---
+++ src/main/java/com/joelk/forohub/domain/user/DataRegistrationUser.java
@@ -0,0 +1,19 @@
+package com.joelk.forohub.domain.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record DataRegistrationUser(
+        @NotBlank(message = "Name is required")
+        String name,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, message = "Password must be at least 6 characters long")
+        String password
+) {
+}
