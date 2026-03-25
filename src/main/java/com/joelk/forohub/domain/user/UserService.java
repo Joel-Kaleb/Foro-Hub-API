1e10aaf0e07a17d56c51d579a42dac345327327e
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-03-25 11:55:31 -0600

    feat(user): create service for user

---
+++ src/main/java/com/joelk/forohub/domain/user/UserService.java
@@ -0,0 +1,9 @@
+package com.joelk.forohub.domain.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
