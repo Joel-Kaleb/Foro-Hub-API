f03161ff6fe49729621a851109bb128e8d5f2276
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-03-25 12:36:37 -0600

    feat(userService): add function to new user

--- src/main/java/com/joelk/forohub/domain/user/UserService.java
+++ src/main/java/com/joelk/forohub/domain/user/UserService.java
@@ -1,9 +1,26 @@
 package com.joelk.forohub.domain.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public DataDetailUser createNewUser(DataRegistrationUser data) {
+        var hashedPassword = passwordEncoder.encode(data.password());
+
+        var user = new User(data, hashedPassword);
+
+        userRepository.save(user);
+
+        return new DataDetailUser(user);
+    }
 }
