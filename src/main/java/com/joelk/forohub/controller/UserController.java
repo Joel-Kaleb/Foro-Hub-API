49259a3dfd2d3fecf4b4f67c3606a603dbd27aa3
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-03-25 13:11:01 -0600

    feat(controller): add function to new user

--- src/main/java/com/joelk/forohub/controller/UserController.java
+++ src/main/java/com/joelk/forohub/controller/UserController.java
@@ -1,9 +1,29 @@
 package com.joelk.forohub.controller;
 
+import com.joelk.forohub.domain.user.DataRegistrationUser;
+import com.joelk.forohub.domain.user.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity newUser(@RequestBody @Valid DataRegistrationUser user, UriComponentsBuilder uriBuilder) {
+        var response = userService.createNewUser(user);
+
+        var uri = uriBuilder.path("/users/{id}/").buildAndExpand(response.id()).toUri();
+
+        return ResponseEntity.created(uri).body(response);
+    }
 }
