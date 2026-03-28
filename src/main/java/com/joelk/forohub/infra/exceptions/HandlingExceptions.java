45edd185171e81671c09272ca10ac0e82bd2f82d
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-03-27 19:18:53 -0600

    fix(infra/security): prevent info disclosure by handling HttpMessageNotReadableException

--- src/main/java/com/joelk/forohub/infra/exceptions/HandlingExceptions.java
+++ src/main/java/com/joelk/forohub/infra/exceptions/HandlingExceptions.java
@@ -3,6 +3,7 @@ package com.joelk.forohub.infra.exceptions;
 import com.joelk.forohub.domain.ValidationException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,6 +21,12 @@ public class HandlingExceptions {
     public ResponseEntity erro400Client(MethodArgumentNotValidException ex) {
         var errors = ex.getFieldErrors();
         return ResponseEntity.badRequest().body(errors.stream().map(DataValidationError::new).toList());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity handle400Jackson(HttpMessageNotReadableException ex) {
+        // Esto atrapará cualquier error de lectura de JSON antes de la validación
+        return ResponseEntity.badRequest().body("The JSON body is malformed or has invalid data types.");
     }
 
     @ExceptionHandler(ValidationException.class)
